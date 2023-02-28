3aa2158d1be9f11a2523938b16490fe2e956c024
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-02-28 08:34:29 +0000

    added tests directory

---
+++ __tests__/api.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import PlanetProvider, { SpaceContext } from ".";
+
+it("should render the current planet name", () => {
+  const TestComponent = () => {
+    const { currPlanet } = React.useContext(SpaceContext);
+    return <div>{currPlanet}</div>;
+  };
+
+  const { getByText } = render(
+    <PlanetProvider>
+      <TestComponent />
+    </PlanetProvider>
+  );
+
+  expect(getByText("Moon")).toBeInTheDocument();
+});
